699e57c0c114785d0fc928ebd758652e08764cbf
John Kamau <johnwaweru092@gmail.com>
2025-03-23 23:14:12 +0300

    implemented all crud operations

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -14,15 +14,17 @@ class TaskController extends Controller
         // Get All Tasks
         $tasks = Task::get();
         if($tasks->count() > 0){
-            return TaskResource::collection($tasks);
+            return response()->json([
+                "message" => "Tasks found",
+                "data" => TaskResource::collection($tasks)
+            ], 200);
         } else {
-            return response()->json(["message" => "No tasks available"], 200);
+            return response()->json(["message" => "No tasks available"], 204);
         }
     }
 
     public function store(Request $request){
         // Store a task function
-
         $validator = Validator::make($request->all(), [
             'title' => 'required|string|max:255',
             'description' => 'nullable|string',
@@ -51,15 +53,47 @@ class TaskController extends Controller
         ], 201);
     }
 
-    public function show(){
-        // Get All Tasks
+    public function show(Task $task){
+        // Get Single Task
+        return new TaskResource($task);
     }
 
-    public function update(){
-        // Get All Tasks
+    public function update(Request $request, Task $task){
+        // Update Single Task
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'priority' => 'nullable|string|in:low,medium,high',
+            'due_date' => 'nullable'
+        ]);
+    
+        if($validator->fails()){
+            return response()->json([
+                'message' => 'Error updating the task. Check the fields.',
+                'error' => $validator->messages()
+            ], 422);
+        }
+
+        // if validation passes, update the task
+        $task->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'priority' => $request->priority,
+            'due_date' => $request->due_date
+        ]);
+
+        return response()->json([
+            'message' => "Task updated successfully",
+            'data'=> new TaskResource($task)
+        ], 200);
+
     }
 
-    public function destroy(){
-        // Get All Tasks
+    public function destroy(Task $task){
+        // Delete a task
+        $task->delete();
+        return response()->json([
+            "message" => "Task deleted successfully"
+        ], 200);
     }
 }
