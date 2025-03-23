94c5000d46668f47cab54adde3dd6e5d4c8c276f
John Kamau <johnwaweru092@gmail.com>
2025-03-23 22:12:42 +0300

    tasks migrations and model

---
+++ app/Models/Task.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tasks';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'priority',
+        'due_date'
+    ];
+
+    protected $attributes = [
+        'priority' => 'medium'
+    ];
+
+}
