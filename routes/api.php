6ad275caaf64fc9a9b8ba5bdf9addae1306109fc
John Kamau <johnwaweru092@gmail.com>
2025-03-23 23:16:09 +0300

    added comment to the api route

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TaskController;
 
 
+/*
+  GET        api/tasks .................................... tasks.index › Api\TaskController@index
+  POST            api/tasks .................................... tasks.store › Api\TaskController@store
+  GET        api/tasks/{task} ............................... tasks.show › Api\TaskController@show
+  PUT|PATCH       api/tasks/{task} ........................... tasks.update › Api\TaskController@update
+  DELETE          api/tasks/{task} ......................... tasks.destroy › Api\TaskController@destroy
+*/
 Route::apiResource('tasks',TaskController::class);
 
 Route::get('/user', function (Request $request) {
